9bfff1b218c6704ac5e07ceddb227d75c73622fb
Madeleine Kemeny <madeleinekemeny@gmail.com>
2019-05-16 23:09:32 -0500

    more refined js with a few bells abnd whistles/no pop-ups

--- assets/js/crystals.js
+++ assets/js/crystals.js
@@ -5,6 +5,7 @@ var losses = 0;
 var sum = 0;
 var imageArray = ["./assets/images/sapphire.png","./assets/images/emerald.png","./assets/images/topaz.png","./assets/images/amethyst.png"]
 var imageValue=[]
+var showGameOver=false; //indicates end before reset; the flag was a Phil addition; we use it later to trick the computer into continuing to play regardless of win/loss condition
 
 // cleans html 
 $("#wins").html(" <b>"+wins);
@@ -13,8 +14,7 @@ $("#losses").html(" <b>"+losses);
 // function defined: generate a random number between 19-120 and display in html.
 function computerCalculation(){
   computerNumber = Math.floor(Math.random() * 101) + 19;
-
-  $("#computerNumber").html(" " +computerNumber);
+   $("#computerNumber").html(" " +computerNumber);
 }
 
 // function defined: when the games begins, do the following...
@@ -37,25 +37,23 @@ function startGame(){
     // addition of class ".crystal-image" to facilitate on-click event
     imageCrystal.addClass("crystal-image gems");
 
-    // connecting array index to image files; console log index number instead of random number to prevent cheating
+    // connecting array index to image files; console log index number instead of random number to prevent cheating ( 12 yr old made this necessary; Isabel explained how to substitute the values)
     imageCrystal.attr("src",imageArray[i]);
     console.log(imageArray[i]);
 
     // adding attribute "data-crystalValue" 
-    imageCrystal.attr("data-crystalvalue", i);
+    imageCrystal.attr("data-crystalvalue", gemRandomNumber);
 
     // assign images to #gembox and assign random number to each index of array
     $("#gembox").append(imageCrystal);
-    imageValue[i] = gemRandomNumber
   }
-    // hidden to prevent cheating as random number is stated: console.log(imageValue)
 
-  // listening for click to begin sum calculation
+   // listening for click to begin sum calculation
   $(".crystal-image").on("click", function() {
     
-    // keep in data-crystalvalue the index of the array
-    var index = ($(this).attr("data-crystalvalue"));
-    var crystalValue = imageValue[index]
+    // storing random number for each image
+    var numberFromImage = ($(this).attr("data-crystalvalue"));
+    var crystalValue =  numberFromImage
 
     // also hidden to prevent cheating as random number is stated:: console.log(crystalValue)
     crystalValue = parseInt(crystalValue);
@@ -66,32 +64,44 @@ function startGame(){
     // displays sum in html
     $("#sum").html(" <b>"+sum);
   
-    // win: calculation of clicks equals random computer number; adds +1 to "wins"; alerts user to "win"
-    if (sum === computerNumber) {
+    // win: calculation of clicks equals random computer number; adds +1 to "wins"
+    if (sum === computerNumber  ) {
       wins++;
       $("#wins").html(" <b>"+wins);
-      alert(`Nice work! You won. Your sum is: ${sum}`)
+  
       // begin game without resetting wins/losses
-      start()
-     
+      showGameOver=true;
     }
 
-    // win: calculation of clicks > random computer number; adds +1 to "losses"; alerts user to "loss"
-    else if (sum >= computerNumber) {
+    // win: calculation of clicks > random computer number; adds +1 to "losses"
+    else if (sum >= computerNumber && showGameOver===false) {
       losses++;
       $("#losses").html(" <b>"+losses);
-      alert(`Your number is too big! You lose. Your sum is: ${sum}`)
-        // begin game without resetting wins/losses
-        start()
+      showGameOver=true;
     }
 
+    // independent statement next executed to restart the game at next click regardless of win/loss
+    else {
+      if(showGameOver===true && sum != computerNumber) {
+        resetGame();
+      }
+    }
   });
 }
 
-function start(){
-// initiate the variables at start
+// initiate only these variables at reset
+function resetGame() {
   computerCalculation()
   startGame()
+  sum=0;
+  $("#sum").html(" <b> 0");
+  showGameOver=false;
+}
+
+// initiate all variables at start
+function start(){
+computerCalculation()
+startGame()
 }
 
 // run local scopes
